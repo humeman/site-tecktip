35951db7f03b2fdb19e79e3692b3d1029f9f0ce4
humeman <camdenjsf@gmail.com>
2024-09-24 12:55:24 -0500

    Un-chaos mode

--- src_api/api/blueprints/panel/teckgpt.py
+++ src_api/api/blueprints/panel/teckgpt.py
@@ -21,20 +21,20 @@ openai_client = openai.AsyncOpenAI(
 )
 
 default_instructions = """
-Your goal is to write "teck tips". 
-Whenever someone asks for a teck tip, respond using the format described below. 
 Note that correct grammar is NOT to be used, and most words should be badly misspelled. 
 Add characters like {}[],.<>/? in random places, within words or as punctuation. 
 If you wish, tips don't have to make sense, and they can sometimes loosely contain an illogical technology joke.
 Be sure to begin your message with a helpful greeting, for example "teck tip today!!!!" or "teck!!!,"
-Tips must be no longer than 128 characters.
-Respond with only one teck tip per message from the user.
-Do not duplicate tips you have seen previously.
-Do not use any highly offensive words.
 """
 
 prompt = """
+Your goal is to write "teck tips". 
+Whenever someone asks for a teck tip, respond using the format described below. 
 %instructions%
+Tips must be no longer than 128 characters.
+Respond with only one teck tip per message from the user.
+Do not duplicate tips you have seen previously.
+Do not use any offensive language.
 """
 
 
@@ -58,7 +58,7 @@ async def generate_tip():
     rate_limits[addr] = time.time()
     user_prompt = request.args.get("prompt")
     if user_prompt is not None and len(user_prompt) > 3:
-        if len(user_prompt) > 500:
+        if len(user_prompt) > 1000:
             return {
                 "success": False,
                 "reason": "TeckGPT has refused your request because your demands are far too lengthy. Please fix this."
