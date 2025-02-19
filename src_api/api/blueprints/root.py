3daa38bab173b6ebe1798a814a1e45590232330d
humeman <camdenjsf@gmail.com>
2025-02-18 22:09:12 -0600

    Additional niceness

--- src_api/api/blueprints/root.py
+++ src_api/api/blueprints/root.py
@@ -10,7 +10,7 @@ import base64
 root_blueprint = Blueprint("root_page", __name__)
 rate_limits = {}
 
-banned_words = [base64.b64decode(x).decode().strip() for x in ["ZnVjawo=", "c2hpdAo=", "Yml0Y2gK", "cmVwZW50Cg==", "YXNzCg==", "c2V4Cg==", "aGFtbWFtCg=="]]
+banned_words = [base64.b64decode(x).decode().strip() for x in ["ZnVjawo=", "c2hpdAo=", "Yml0Y2gK", "cmVwZW50Cg==", "YXNzCg==", "c2V4Cg==", "aGFtbWFtCg==", "d29vZHMK", "c291bAo="]]
     
 
 image_url = os.getenv("IMAGE_URL")
@@ -47,6 +47,9 @@ async def random_nice_tip():
             if banned_word.lower() in tip.tip.lower():
                 tip = None
                 break
+            
+        if "teck" not in tip.tip.lower():
+            tip = None
 
     if request.content_type == "application/json":
         return tip.as_dict()
